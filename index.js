8ac2c4c609bb323118559be48b600b431424a1f6
cleanerzkp <104455005+cleanerzkp@users.noreply.github.com>
2023-02-05 15:47:58 +0100

    splited into 3 files

--- index.js
+++ index.js
@@ -1,48 +1,34 @@
 const form = document.querySelector('form');
-const resultsDiv = document.querySelector('#results');
+const output = document.querySelector('#output');
 
-form.addEventListener('submit', (event) => {
+form.addEventListener('submit', async (event) => {
   event.preventDefault();
+  const numBlocks = form.elements.numBlocks.value;
+  const hashStart = form.elements.hashStart.value;
 
-  const blockNumber = form.elements.blockNumber.value;
-  const firstCharacter = form.elements.firstCharacter.value;
-
-  // Validate input
-  if (blockNumber < 0 || blockNumber > 100 || firstCharacter.length !== 1) {
-    return;
+  output.textContent = '';
+  let prevBlockHash = '';
+  for (let i = 1; i <= numBlocks; i++) {
+    let block = {
+      nonce: 0,
+      hash: ''
+    };
+    while (!block.hash.startsWith(hashStart)) {
+      block.nonce++;
+      block.hash = await hash(`${prevBlockHash}${block.nonce}`);
+    }
+    output.textContent += `Block: Block ${i}\n`;
+    output.textContent += `Nonce: ${block.nonce}\n`;
+    output.textContent += `Hash: ${block.hash}\n\n`;
+    prevBlockHash = block.hash;
   }
-
-  // Clear previous results
-  resultsDiv.innerHTML = '';
-
-  const crypto = require('crypto');
-
-const hash = crypto.createHash('sha256');
-hash.update('your data to hash');
-const hashValue = hash.digest('hex');
-
-const findBlock = (blockNumber, prevBlockHash) => {
-  let nonce = 0;
-  let hash = '';
-
-  while (!hash.startsWith('5')) {
-    hash = crypto.createHash('sha256').update(blockNumber + nonce + prevBlockHash).digest('hex');
-    nonce++;
-  }
-
-  return {
-    nonce,
-    hash
-  };
-};
-
-let prevBlockHash = '0';
-for (let i = 0; i < 3; i++) {
-  const block = findBlock(i, prevBlockHash);
-  console.log(`Block: Block ${i}`);
-  console.log(`Nonce: ${block.nonce}`);
-  console.log(`Hash: ${block.hash}`);
-  prevBlockHash = block.hash;
-}
-
 });
+
+async function hash(data) {
+  const buffer = new TextEncoder().encode(data);
+  const hashArray = await window.crypto.subtle.digest('SHA-256', buffer);
+  const hashHex = Array.from(new Uint8Array(hashArray))
+    .map(b => b.toString(16).padStart(2, '0'))
+    .join('');
+  return hashHex;
+}
